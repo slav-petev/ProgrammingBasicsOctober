7cd88b36af26d8470e5765494155b5da5c044b19
slav_ <slav_@DESKTOP-JQR7HUT>
2016-11-06 03:17:31 -0800

    Solved first three problems from Complex Conditional Statements

---
+++ ProgrammingBasicsOctober/03.PointInRectangle/PointInRectangleExcercise.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.PointInRectangle
+{
+    class PointInRectangleExcercise
+    {
+        static void Main(string[] args)
+        {
+            var upperLeftPointXCoordinate =
+                double.Parse(Console.ReadLine());
+            var upperLeftPointYCoordinate =
+                double.Parse(Console.ReadLine());
+            var lowerRightPointXCoordinate =
+                double.Parse(Console.ReadLine());
+            var lowerRightPointYCoordinate =
+                double.Parse(Console.ReadLine());
+            var pointToCheckXCoordinate =
+                double.Parse(Console.ReadLine());
+            var pointToCheckYCoordinate =
+                double.Parse(Console.ReadLine());
+
+            bool isPointHorizontallyInside =
+                upperLeftPointXCoordinate <=
+                pointToCheckXCoordinate &&
+                pointToCheckXCoordinate <=
+                lowerRightPointXCoordinate;
+            bool isPointVerticallyInside =
+                upperLeftPointYCoordinate <=
+                pointToCheckYCoordinate &&
+                pointToCheckYCoordinate <=
+                lowerRightPointYCoordinate;
+
+            bool isPointInside =
+                 isPointHorizontallyInside &&
+                 isPointVerticallyInside;
+
+            if (isPointInside)
+            {
+                Console.WriteLine("Inside");
+            }
+            else
+            {
+                Console.WriteLine("Outside");
+            }
+        }
+    }
+}
