4c20829b182051560aa536d31344771ade264a74
slav_ <slav_@DESKTOP-JQR7HUT>
2016-11-10 22:23:16 -0800

    Added projects for all problems and almost solved them all

---
+++ SimpleLoops/10.HalfSumElement/HalfSumElementExercise.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.HalfSumElement
+{
+    class HalfSumElementExercise
+    {
+        static void Main(string[] args)
+        {
+            var numbersCount = int.Parse(
+                Console.ReadLine());
+
+            var maxNumber = int.MinValue;
+            var sumElements = 0;
+
+            for (var i = 0;
+                i < numbersCount;
+                i++)
+            {
+                var currentNumber = int.Parse(
+                    Console.ReadLine());
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+                else
+                {
+                    sumElements += currentNumber;
+                }
+            }
+
+            if (maxNumber == sumElements)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = {0}",
+                    sumElements);
+            }
+            else
+            {
+                var difference = Math
+                    .Abs(maxNumber - sumElements);
+
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = {0}",
+                    difference);
+            }
+        }
+    }
+}
